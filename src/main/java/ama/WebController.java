ace7d1902cd85326468d15470fb2afe66bea3569
Holpa <ahmadholpa@cmail.carleton.ca>
2017-02-07 17:58:05 -0500

    Added static home page

--- src/main/java/ama/WebController.java
+++ src/main/java/ama/WebController.java
@@ -1,17 +1,20 @@
 package ama;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by robertfernandes on 2/7/2017.
  */
-@RestController
+@Controller
 public class WebController {
 
-    @RequestMapping("/")
-    public String hi() {
-        return "hi";
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
