804aba3d245d5a7ffbb4678bfb3896b00e5e6431
robertfernandes <ratminer_54321@hotmail.com>
2017-02-07 17:02:56 -0500

    commmmit

--- src/main/java/ama/AMAApplication.java
+++ src/main/java/ama/AMAApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Created by robertfernandes on 2/7/2017.
+ * Created by robertfernandes on 2/7/2017.ss
  */
 @SpringBootApplication
 public class AMAApplication {
