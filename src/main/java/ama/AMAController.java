d63929c382ca10cc15e714363d3c9f82798425e2
nursh <nursh@github.com>
2017-03-01 10:58:39 -0500

    created AMA controller

--- src/main/java/ama/AMAController.java
+++ src/main/java/ama/AMAController.java
@@ -1,8 +1,22 @@
 package ama;
 
-import org.springframework.web.bind.annotation.RestController;
+import ama.Model.Category;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
-@RestController
+@Controller
 public class AMAController {
+
+    @GetMapping("/home")
+    public String getHomePage() {
+        return "AMAHomePage";
+    }
+
+    @GetMapping("/create")
+    public String getCreationPage(Model m) {
+        m.addAttribute("questionCategories", Category.getValues());
+        return "AMACreationPage";
+    }
 }
