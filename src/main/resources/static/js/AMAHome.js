96d9edcf8d28470097ed09b6bfdcf92f7738d576
nursh <nursh@github.com>
2017-03-06 21:19:18 -0500

    Ahmad, Fixed your ajax call

--- src/main/resources/static/js/AMAHome.js
+++ src/main/resources/static/js/AMAHome.js
@@ -1,18 +1,11 @@
 /**
  * Created by ahmadholpa on 3/2/2017.
  */
-$(document).ready(function() {
+
 
     $.ajax({
         url: "http://rest-service.guides.spring.io/greeting"
     }).then(function(data) {
-        /*
-        for (i=0; data.length;i++) {
-            $('.AMA-author').append(data[i].author);
-            $('.AMA-description').append(data[i].description);
-        }
-        */
         $('.AMA-author').append(data.id);
         $('.AMA-description').append(data.content);
     });
-});
