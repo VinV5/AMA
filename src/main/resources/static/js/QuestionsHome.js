b25e821619e3e60d6103a6a2aae8d24947553149
Holpa <ahmadholpa@cmail.carleton.ca>
2017-03-13 00:18:17 -0400

    This branch will add Question answer feature.
    1- Answer Repo added but not wired
    
    2- QuestionsHome.js contains nested AJAX calls
    
    3- AMAController has redundency calls.
    
    4- need to implement class ANSWER with QUESTION accordingly.
    
    5- BIG issue ... with adding Answer Button....
    
    GG getRekt

--- src/main/resources/static/js/QuestionsHome.js
+++ src/main/resources/static/js/QuestionsHome.js
@@ -1,16 +1,39 @@
 
     var amaid = $('#amaid').text();
     var list = $('.questionlist');
+    var listAnswer =$('.answerlist');
+    listAnswer.empty();
     list.empty();
+
+
     $.ajax({
      url: "/ama/"  + amaid + "/questions"
     }).then(function(data){
             $.each(data, function(key, value) {
                 if(value.content !== "") {
-                    var par = '<div class="well">';
+                 var par = '<div class="well">';
                     par += '<p> '+ value.content +'</p>';
+
+                    if(value.answer =="")
+                    {
+                        alert("passed value.answer !==" + value.answer);
+                       $.ajax({
+                        url: "/ama/"  + amaid + "/questions/" + amaid +"/question"
+                       }).then(function(data2){
+                               $.each(data2, function(key2, value2) {
+                                   if(value2.content !=="")
+                                   {
+                                       par += '<p> '+ value2.content +'</p>';
+                                   }
+                               });// end  each loop
+                       });// end data function
+                    }
                     par += '</div>';
                     list.append(par);
-                }
-            });
-    });
+                }// end if
+            });// end  each loop
+    });// end data function
+function myFunction()
+{
+    alert("hai");
+}
