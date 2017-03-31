2bd20e4ab94e3e9fb0b0d2f4b8c1fd9dd51de988
Holpa <ahmadholpa@cmail.carleton.ca>
2017-03-31 00:56:59 -0400

    rip

--- src/main/resources/static/js/solo.js
+++ src/main/resources/static/js/solo.js
@@ -14,3 +14,22 @@ $('.question').click(function(e) {
     }
     $('input').toggleClass('hidden');
 })
+$('#menu-toggle').click( function(){
+    $(this).find('i').toggleClass('glyphicon-arrow-right').toggleClass('glyphicon-arrow-left');
+});
+
+function foo(){
+    if( typeof foo.counter == 'undefined' ) {
+        foo.counter = 0;
+    }
+    foo.counter++;
+    alert(foo.counter);
+}
+
+$(document).ready( function(){
+//        var logFlag= true;
+//        alert(logFlag);
+//        $(".navbar-right").append("<li><a href=\"login\"><span id=\"menu-login\" class=\"glyphicon glyphicon-log-in \"></span> Login</a></li>");
+////            $('.navbar-nav navbar-right').append("<li><a href=\"logout\"><span id=\"menu-logout\" class=\"glyphicon glyphicon-log-out \"></span> Logout</a></li>");
+//        logFlag = false;
+});
