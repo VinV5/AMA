a17916860513593c367f9276f7baa129fff8fd4e
Robert Fernandes <ratminer_54321@hotmail.com>
2017-04-04 00:44:44 -0400

    Revert "Revert "Revert "Feature answer"""

--- src/main/java/ama/repositories/QuestionRepository.java
+++ src/main/java/ama/repositories/QuestionRepository.java
@@ -1,11 +1,8 @@
 package ama.repositories;
 
-import ama.model.*;
+import ama.model.Question;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 
 public interface QuestionRepository extends CrudRepository<Question, Long>{
     Question findById(Long id);
-    List<Question> findByAma(AMA ama);
 }
